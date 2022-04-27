25c0ab4c3f4e8c79669e0e79d99557d013446ad9
snehasonwane <snehasonwane01@gmail.com>
2022-04-27 23:12:12 +0530

    using switch case

--- Emppresent/Emppresent/Program.cs
+++ Emppresent/Emppresent/Program.cs
@@ -1,7 +1,7 @@
 ﻿class Program
-{ 
-   static void Main(string[] args)
 {
+    static void Main(string[] args);
+
         //constants
         //int IS_FULL_TIME = 1;
         //Random random = new Random();
@@ -33,7 +33,7 @@
          empWage = empHrs * EMP_RATE_PER_HOUR;
          Console.WriteLine("Emp Wage : + empwage");*/
 
-        int IS_PART_TIME = 1;
+        /* int IS_PART_TIME = 1;
         int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
@@ -53,7 +53,33 @@
             empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Emp Wage :" + empWage);
+        Console.WriteLine("Emp Wage :" + empWage); */
+
+         public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+       public const int EMP_RATE_PER_HOUR = 20;
+ 
+      static void Main(string[] args)   
+    {
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+        int empCheck = random.Next(0, 3);
+        switch (empCheck)
+        {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+                break;
+
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("emp Wage :" + empWage);
 
 
     }
