89b83bf3f6d96526c6e55536d3b4fd64fdfc9705
snehasonwane <snehasonwane01@gmail.com>
2022-04-27 22:28:24 +0530

    calculate daily wage

--- Emppresent/Emppresent/Program.cs
+++ Emppresent/Emppresent/Program.cs
@@ -3,18 +3,36 @@
    static void Main(string[] args)
 {
     //constants
-    int IS_FULL_TIME = 1;
-    Random random = new Random();
-    //computaaion
-    int empCheck = random.Next(0, 2);
-    if (empCheck == IS_FULL_TIME)
-        {
-            Console.WriteLine("Employee is present");
-        }
-    else
-        {
-            Console.WriteLine("Employee is  absent");
-        }
+    //int IS_FULL_TIME = 1;
+    //Random random = new Random();
+    ////computaaion
+    //int empCheck = random.Next(0, 2);
+    //if (empCheck == IS_FULL_TIME)
+    //    {
+    //        Console.WriteLine("Employee is present");
+    //    }
+    //else
+    //    {
+    //        Console.WriteLine("Employee is  absent");
+    //    }
 
-}
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empWage = 0;
+        int empHrs = 0;
+        Random obj = new Random();
+        int empCheck = obj.Next(0, 3);
+        if (empCheck == IS_FULL_TIME)
+        {
+            empHrs = 8;
+        }
+        else
+        {
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage : + empwage");
+
+
+    }
 }
